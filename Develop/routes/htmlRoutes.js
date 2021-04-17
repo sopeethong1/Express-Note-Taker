2799321b5cac0f9205516066070da25b88c385b6
Sopee Thong <tsopee@gmail.com>
2021-04-17 02:43:30 -0400

    updating notes path

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 module.exports = (app) => {
   app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, '..public/notes.html'));
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
   });
  
   app.get("*", function (req, res) {
