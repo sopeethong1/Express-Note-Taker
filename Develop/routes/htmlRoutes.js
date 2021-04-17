a1431645eee18e447526552104fb565f603a69c4
Sopee Thong <tsopee@gmail.com>
2021-04-17 02:56:09 -0400

    index update

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -6,11 +6,10 @@ const path = require("path");
 
 module.exports = (app) => {
   app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, 'public','notes.html'));
   });
  
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./index.html"));
-  });
+    res.sendFile(path.join(__dirname, './index.html'));
 };
 
