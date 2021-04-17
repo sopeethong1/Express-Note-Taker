af308421a3b4fb58c802bc081e1e91ba0ddbe2e2
Sopee Thong <tsopee@gmail.com>
2021-04-17 01:57:01 -0400

    removing dup code

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -9,12 +9,6 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
     console.log("GET request for /notes was successful.");
   });
-
-  app.get("/api/notes", function (req, res) {
-    console.log("GET request for /api/notes was successful.");
-    return res.sendFile(path.json(__dirname, "db/db.json"));
-  });
-
  
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
